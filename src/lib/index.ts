9b6c15d4a0736c30edc79ad916bb69a6be76114a
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-03-11 05:19:55 +0700

    ref: svelte image lazy loading

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,8 @@
 import LogicState from './components/logicState/LogicState.svelte'
 
+export { lazyimage, type LazyImageOptions } from 'svelte-lazyimage-cache'
 export { persisted } from 'svelte-local-storage-store'
 export { persist as formPersisted } from 'svelte-use-persist'
-export { lazyimage } from './actions/useLazyImage'
 
 export { LogicState }
 
