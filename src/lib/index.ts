9febc60e286cc094e1e94d1f4273868d8be417e3
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-03-11 04:57:45 +0700

    feat: add persisted

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 import LogicState from './components/logicState/LogicState.svelte'
 
+export { persisted } from 'svelte-local-storage-store'
 export { lazyimage } from './actions/useLazyImage'
 
 export { LogicState }
