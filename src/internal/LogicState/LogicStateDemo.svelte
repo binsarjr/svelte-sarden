049859744f212599decefe3b851aba3d25449b66
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-03-11 03:58:22 +0700

    add demo default as true in LogicState

--- src/internal/LogicState/LogicStateDemo.svelte
+++ src/internal/LogicState/LogicStateDemo.svelte
@@ -23,3 +23,27 @@
 		</tr>
 	</table>
 </LogicState>
+
+<!-- if you wanna set true as default -->
+<LogicState state={true} let:state let:setTrue let:setFalse let:toggle>
+	<table>
+		<tr>
+			<td>Status</td>
+			<td>{state}</td>
+		</tr>
+		<tr>
+			<td>
+				<button on:click={setTrue}>setTrue</button>
+			</td>
+			<td>
+				<button on:click={() => setFalse()}>setFalse</button>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<button on:click={toggle}>Toggle</button>
+			</td>
+		</tr>
+	</table>
+</LogicState>
+
