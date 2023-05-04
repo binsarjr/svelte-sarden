75ec2f30c62d24da7435d8265074c5076a8727c4
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-05-05 04:40:06 +0700

    add: click to scroll

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,7 @@ import LogicState from './components/logicState/LogicState.svelte'
 export * from 'svelte-lazyimage-cache'
 export * from 'svelte-local-storage-store'
 export { persist as formPersisted } from 'svelte-use-persist'
+export * from './actions/clickToScroll'
 
 export { LogicState }
 
